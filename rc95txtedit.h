95e0e44bce5cecdd3d5917bc4a1e1805676d1794
Chehot's Republic of CATV 95 <ChehotsRepublicOfCATV95@users.noreply.github.com>
2018-07-18 12:36:06 +0000

    [TXTEDIT] Correct Indentations

--- rc95txtedit.h
+++ rc95txtedit.h
@@ -1,26 +1,26 @@
-#define TXTEDIT_TITLE "Text Editor 4.0"
-#define TXTEDIT_ICONNAME "TEXTEDIT.ICO"
-#define TXTEDIT_WIDTH 640
-#define TXTEDIT_HEIGHT 480
-#define IDC_MAIN_EDIT 1001
-#define IDR_MENUS 101
-#define IDI_ICONS 201
+#define TXTEDIT_TITLE			"Text Editor 4.0"
+#define TXTEDIT_ICONNAME		"TEXTEDIT.ICO"
+#define TXTEDIT_WIDTH			640
+#define TXTEDIT_HEIGHT			480
+#define IDC_MAIN_EDIT			1001
+#define IDR_MENUS				101
+#define IDI_ICONS				201
 
-#define ID_FILE_NEW		9000
-#define ID_FILE_EXIT	9001
-#define ID_FILE_OPEN	9002
-#define ID_FILE_SAVE	9003
-#define ID_EDIT_CUT		9004
-#define ID_EDIT_COPY	9005
-#define ID_EDIT_PASTE	9006
-#define ID_EDIT_UNDO	9007
-#define IDR_MENUS 101
-#define IDI_ICONS 201
+#define ID_FILE_NEW				9000
+#define ID_FILE_EXIT			9001
+#define ID_FILE_OPEN			9002
+#define ID_FILE_SAVE			9003
+#define ID_EDIT_CUT				9004
+#define ID_EDIT_COPY			9005
+#define ID_EDIT_PASTE			9006
+#define ID_EDIT_UNDO			9007
+#define IDR_MENUS				101
+#define IDI_ICONS				201
 
-#define ID_HELP_ABOUT	9008
-#define IDD_ABOUT		9009
-#define IDD_DIALOG		9010
+#define ID_HELP_ABOUT			9008
+#define IDD_ABOUT				9009
+#define IDD_DIALOG				9010
 
-#define None NULL
+#define None					NULL
 
 
